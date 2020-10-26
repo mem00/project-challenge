57fb98c0ac280ccaafb2e7b84037d142e52b10ba
Michael McGuire <michaelmcguire@michaels-mbp.myfiosgateway.com>
2020-10-26 12:24:54 -0400

    update order_by_likes_last_hour

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,6 +5,6 @@ class Dog < ApplicationRecord
   has_many :dog_likes
 
   def self.ordered_likes_last_hour
-    left_joins(:dog_likes).group(:id).where('dog_likes.created_at >= ?', DateTime.now - 1.day).order('COUNT(dog_likes.id) DESC')
+    left_joins(:dog_likes).where('dog_likes.created_at >= ?', DateTime.now - 1.hour).group(:id).order('COUNT(dog_likes.id) DESC')
   end
 end
